1368a8000b6b87c217fc0557bab0a02993e539b2
Michael Vu <ducva@arrow-tech.vn>
2016-01-23 03:57:43 +0700

    second: first state

---
+++ www/js/HomeController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by michael on 23/01/2016.
+ */
+angular.module('starter.controllers', [])
+
+  .controller('HomeCtrl', function($scope) {
+    $scope.model = {};
+    $scope.model.name = "World";
+
+  });
